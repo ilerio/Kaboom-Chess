4528aed6eaca3280e00ab344a461a012a19064b8
Ilerioluwa Oyedele <ilerioyedele@gmail.com>
2021-10-16 14:32:00 -0600

    moved asset loading into separate file

--- game.js
+++ game.js
@@ -1,30 +1,15 @@
 import kaboom from "https://unpkg.com/kaboom@next/dist/kaboom.mjs";
-import {dlog, clog} from "./helpers.js" // TODO: put all function definitions in here
+import {dlog, clog} from "./helpers.js"
+import {loadAssets} from "./setup.js"
+//import {} from "./board.js"
+//import {} from "./peices.js"
 
 kaboom({
   global: true,
   background: [ 0, 0, 0, ],
 });
 
-loadSprite("bbishop", "sprites/bbishop.png");
-loadSprite("bking", "sprites/bking.png");
-loadSprite("bknight", "sprites/bknight.png");
-loadSprite("bpawn", "sprites/bpawn.png");
-loadSprite("bqueen", "sprites/bqueen.png");
-loadSprite("brook", "sprites/brook.png");
-loadSprite("wbishop", "sprites/wbishop.png");
-loadSprite("wking", "sprites/wking.png");
-loadSprite("wknight", "sprites/wknight.png");
-loadSprite("wpawn", "sprites/wpawn.png");
-loadSprite("wqueen", "sprites/wqueen.png");
-loadSprite("wrook", "sprites/wrook.png");
-loadSprite("highlight", "sprites/highlight.png");
-loadSprite("border", "sprites/border.png");
-loadSprite("move", "sprites/move.png");
-loadSprite("attack", "sprites/attack.png");
-
-loadSound("piece_capture", "sounds/piece_capture.mp3");
-loadSound("piece_move", "sounds/piece_move.mp3");
+loadAssets();
 
 scene("main", (args = {}) => {
   layers(["board", "boarder", "peice", "ui"]);
---
+++ setup.js
@@ -0,0 +1,20 @@
+export function loadAssets() {
+  loadSprite("bbishop", "sprites/bbishop.png");
+  loadSprite("bking", "sprites/bking.png");
+  loadSprite("bknight", "sprites/bknight.png");
+  loadSprite("bpawn", "sprites/bpawn.png");
+  loadSprite("bqueen", "sprites/bqueen.png");
+  loadSprite("brook", "sprites/brook.png");
+  loadSprite("wbishop", "sprites/wbishop.png");
+  loadSprite("wking", "sprites/wking.png");
+  loadSprite("wknight", "sprites/wknight.png");
+  loadSprite("wpawn", "sprites/wpawn.png");
+  loadSprite("wqueen", "sprites/wqueen.png");
+  loadSprite("wrook", "sprites/wrook.png");
+  loadSprite("highlight", "sprites/highlight.png");
+  loadSprite("border", "sprites/border.png");
+  loadSprite("move", "sprites/move.png");
+  loadSprite("attack", "sprites/attack.png");
+  loadSound("piece_capture", "sounds/piece_capture.mp3");
+  loadSound("piece_move", "sounds/piece_move.mp3");
+}
